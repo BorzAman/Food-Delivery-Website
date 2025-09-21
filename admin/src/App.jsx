9cf29cd44e40506793e469c7b2ff0a475d28b6c8
Aman Sayyad <amansayyad0012@gmail.com>
2025-09-21 18:58:34 +0530

    Update App.jsx (live backend Url added)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  const url = 'https://food-delivery-website-backend-c4iu.onrender.com/';
 
   return (
     <div>
